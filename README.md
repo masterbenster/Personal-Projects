8d023cb9dc98efac639a10a568f42caa1cb8d3b2
masterbenster <105526615+masterbenster@users.noreply.github.com>
2023-11-16 21:24:44 -0700

    demoAlmostDone

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Personal-Projects
+This repo is intended to showcase the important work I have done as a coder,
+and for me to showcase the projects that I have created.
--- idTechDemo.cpp
+++ idTechDemo.cpp
@@ -2,28 +2,35 @@
 // Created by Brennan Sammis
 // Do not share with anyone
 #include <iostream>
+#include <stdexcept>
+#include <stdio.h>
 #include <iomanip>
 #include <string>
-#include <vector>
+#include <array>
 
-int Main()
+int main()
 {
+    using std::cout;
+    using std::endl;
+
     int count = 0;
+
     bool isIterating = true;
-    int[] array = new int[5];
+    int array[5] = {8,10,15,3,7};
+
     // Note: The array size is 5, this may not always be known
-    for (int i = 0; i < array.size(); i++)
+    for (int i = 0; i < sizeof(array); i++)
     {
-        int[i] = 1;
+        array[i] = 1;
         if (i == 3)
         {
-            int[i] = 0;
+            array[i] = 0;
         }
     }
     // Note: (isIterating==True) is the same as (isIterating)
     while (isIterating)
     {
-        if (count == array.size()) // make sure we are still in our array
+        if (count == sizeof(array)) // make sure we are still in our array
         {
             count = 0; // reset the counter
         }
@@ -34,14 +41,15 @@ int Main()
                 isIterating = false; // set our while loop to false
                 array[count] = -2;
             }
-            count++ // this can also be count = count + 1;
+            count++; // this can also be count = count + 1;
         }
     }
 
     cout << "This is my final array: \n"; 
-    cout <<"Array Values: "
-    for (int i = 0; i < array.size(); i++)
+    cout <<"Array Values: ";
+    for (int i = 0; i < sizeof(array); i++)
     {
-        cout<< i << " ";
+        cout<< array[i] << " ";
     }
+
 }
